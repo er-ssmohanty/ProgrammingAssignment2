7d54e09fff0a4e74f0a3b75e3600b729ea19b010
er-ssmohanty <pie2001@protonmail.com>
2021-07-23 22:02:11 +0530

    Wrote a piece of code to cache the inverse of a matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+##Well I did nothing but just replaced the variables of the original cryptic
+##code with new variables required for matrix inversion
+##Before-->After
+##m-->the_inv
+##setmean-->set_inv
+##getmean-->get_inv
+##mean-->matinv
+##
 makeCacheMatrix <- function(x = matrix()) {
-
+  the_inv <- NULL
+  set <- function(y) {
+    x <<- y
+    the_inv <<- NULL
+  }
+  get <- function() x
+  set_inv <- function(matinv) the_inv <<- matinv
+  get_inv <- function() the_inv
+  list(set = set, get = get,
+       set_inv = set_inv,
+       get_inv = get_inv)
+  
 }
 
-
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  the_inv <- x$getinv()
+  if(!is.null(the_inv)) {
+    message("getting cached data")
+    return(the_inv)
+  }
+  data <- x$get()
+  the_inv <- solve(data)
+  x$set_inv(the_inv)
+  the_inv
 }
+##Have a good day!!
